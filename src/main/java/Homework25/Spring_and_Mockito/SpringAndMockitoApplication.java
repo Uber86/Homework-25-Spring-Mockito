22482e0ae30fb52f5284a35831be147d77f4f17b
Сергей <lockup86@rambler.ru>
2024-09-10 09:22:37 +0300

    hm25

---
+++ src/main/java/Homework25/Spring_and_Mockito/SpringAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package Homework25.Spring_and_Mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Homework25/Spring_and_Mockito/SpringAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package Homework25.Spring_and_Mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
